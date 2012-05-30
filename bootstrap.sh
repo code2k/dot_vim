544703b248fe8d0c4cb48055b77a188ba639aa2b
Andreas Pataki <pataki.andreas@googlemail.com>
2012-05-30 08:23:24 +0200

    symlinks are now created before the first git start

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@
 echo "Installing vundle"
 git clone https://github.com/gmarik/vundle.git bundle/vundle
 
-echo "Initialize vim"
-vim +BundleInstall +qall
-
-echo "Symlink config"
+echo "Symlink vim config"
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gvimrc ~/.gvimrc
+
+echo "Initialize vim"
+vim +BundleInstall +qall
